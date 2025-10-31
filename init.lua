0e602d90439238618fe5cb54f61bdadb7a45d784
larry <segfaulted67@gmail.com>
2025-10-31 12:18:16 +0530

    Removed 'x' map from leap

--- init.lua
+++ init.lua
@@ -120,5 +120,3 @@ vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
 vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
 vim.keymap.set('n',             'gs', '<Plug>(leap-from-window)')
 
-vim.keymap.set({'x', 'o'}, 'x', '<Plug>(leap-forward-till)')
-vim.keymap.set({'x', 'o'}, 'X', '<Plug>(leap-backward-till)')
