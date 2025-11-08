9318000695b7b8504cf242ee3ef767459176e994
larry <segfaulted67@gmail.com>
2025-11-08 19:47:25 +0530

    Added some ui related plugins(dressing, noice, notify)

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,11 @@
+return{
+  'rcarriga/nvim-notify',
+  config = function()
+      local notify = require("notify")
+      vim.notify = notify
+      notify.setup({
+        stages = "fade_in_slide_out",
+        timeout = 3000,
+      })
+    end,
+}
