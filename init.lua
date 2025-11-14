bc73df568a894e49c62f4c42db827a06bc6952f4
larry <segfaulted67@gmail.com>
2025-11-14 22:03:45 +0530

    moved keymaps to a seperate file

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
+require("config.keymap")
 require("plugins.moonfly")
 
 
@@ -71,28 +72,4 @@ vim.opt.encoding = "UTF-8"                         -- Set encoding
 
 -- Cursor settings
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
-
--- keymaps
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-vim.keymap.set({'n', 'i'}, '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set({'n', 'i'}, '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set({'n', 'i'}, '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set({'n', 'i'}, '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- undo-tree
-vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
-
-vim.keymap.set('n', '<C-x><C-f>', vim.cmd.Oil)
-
-vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
-vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
-
-vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
-vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
-vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
 
