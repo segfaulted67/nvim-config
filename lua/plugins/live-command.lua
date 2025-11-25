628da919439661d8fe165e04910fd37cfd29b4fd
larry <segfaulted67@gmail.com>
2025-11-25 23:47:32 +0530

    Added live-command.nvim

---
+++ lua/plugins/live-command.lua
@@ -0,0 +1,12 @@
+return{
+  "smjonas/live-command.nvim",
+  -- live-command supports semantic versioning via Git tags
+  -- tag = "2.*",
+  config = function()
+  require("live-command").setup {
+    commands = {
+      Norm = { cmd = "norm" },
+    },
+  }
+  end,
+}
